7cbc6117fe2ed66a43caf75538a3a5dd74b4be83
Alex Chao <alexchao56@gmail.com>
2015-03-27 10:04:49 -0700

    added a gramFrequency function

---
+++ createGramFrequency.R
@@ -0,0 +1,23 @@
+#Find frequencies for n-grams. Returns a CSV file with a column of grams and their frequencies
+setwd("~/Desktop/CSR_text")
+source("~/Desktop/ClusteringCrowdfunding/createTDM.R")
+allFiles = list.files("corpus/")
+
+gramFrequencyCreator = function(filePath, ngrams) {
+  f = paste("corpus/", filePath, sep="")
+  a = readChar(f, file.info(f)$size)
+  corpus = data.frame(Words = a, stringsAsFactors = F)
+  TDM = createTDM(corpus$Words, "text", ngrams)
+  m = as.matrix(TDM)
+  v = sort(rowSums(m),decreasing=TRUE)
+  d = data.frame(Gram = names(v),Frequency=v)
+  row.names(d) = NULL
+  return(d)
+}
+
+for (i in 1:length(allFiles)) {
+  for (j in 1:2) {
+    fileName = paste(allFiles[i], "_", j, "-Gram", "_", "Frequency.csv", sep = "")
+    write.csv(gramFrequencyCreator(allFiles[i], j), file = fileName)
+  }
+}
